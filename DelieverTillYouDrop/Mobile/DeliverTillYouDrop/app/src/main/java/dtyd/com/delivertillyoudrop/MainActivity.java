6ffbc92be3a048c653736706bbc0efc78e5c3725
David Willis <dwillis@esri.com>
2016-12-15 16:04:42 +0000

    Change FAB icon to something more appropriate. (#7)
    Close action nav. drawer when item is clicked.

--- DelieverTillYouDrop/Mobile/DeliverTillYouDrop/app/src/main/java/dtyd/com/delivertillyoudrop/MainActivity.java
+++ DelieverTillYouDrop/Mobile/DeliverTillYouDrop/app/src/main/java/dtyd/com/delivertillyoudrop/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -48,6 +49,8 @@ public class MainActivity extends AppCompatActivity {
 
   private GraphicsOverlay mGraphicsOverlay;
 
+  private DrawerLayout mDrawerLayout;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -65,6 +68,8 @@ public class MainActivity extends AppCompatActivity {
       }
     });
 
+    mDrawerLayout = (DrawerLayout) findViewById(R.id.content_main);
+
     NavigationView navigationView = (NavigationView) findViewById(R.id.right_drawer);
 
     navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
@@ -78,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
             retVal = false;
             break;
         }
+        mDrawerLayout.closeDrawers();
         return retVal;
       }
     });
